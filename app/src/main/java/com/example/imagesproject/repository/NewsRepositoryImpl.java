29639a832cc3a94d26d6cbbfbb9ce0936eb4ad88
fadi-botros <botros_fadi@yahoo.com>
2020-02-07 18:06:11 +0200

    Update NewsRepositoryImpl.java

--- app/src/main/java/com/example/imagesproject/repository/NewsRepositoryImpl.java
+++ app/src/main/java/com/example/imagesproject/repository/NewsRepositoryImpl.java
@@ -11,11 +11,8 @@ public class NewsRepositoryImpl implements NewsRepository {
     public void getNews(Function<Iterable<String>, Void> callback) {
         // Static data
         callback.apply(Arrays.asList(
-                "https://www.forbes.com/sites/johnkoetsier/2019/10/23/ibm-googles-quantum-supremacy-is-150-million-percent-wrong/",
-                "https://www.forbes.com/sites/forbestechcouncil/2019/06/12/why-sres-should-worry-more-about-third-party-javascript/",
-                "https://www.forbes.com/sites/tjmccue/2020/12/31/lock-down-your-phone-the-new-york-times-privacy-project-revelations/",
-                "https://www.techrepublic.com/article/why-many-security-pros-lack-confidence-in-their-implementation-of-zero-trust/",
-                "https://www.weforum.org/agenda/2019/09/our-cities-are-increasingly-vulnerable-to-cyberattacks-heres-how-they-can-fight-back/"));
+                /* Put name of sites to get their images here */
+                "https://www.forbes.com/sites/johnkoetsier/2019/10/23/ibm-googles-quantum-supremacy-is-150-million-percent-wrong/"
     }
 
 }
